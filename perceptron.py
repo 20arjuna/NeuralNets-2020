9bf98294ed42c4a5106b807f2797f320eca4bcea
Arjun Akkiraju <20arjuna@students.harker.org>
2020-03-02 10:45:42 -0800

    added constructor

--- perceptron.py
+++ perceptron.py
@@ -1,1 +1,8 @@
-print(hex)
+import array as arr
+import numpy as np
+class perceptron:
+    def __init__(self, inputNodes, hiddenLayerNodes, outputNodes):
+        self.inputNodes = 2
+        self.hiddenLayerNodes = np.empty(2)
+        self.outputNodes = 2
+        
